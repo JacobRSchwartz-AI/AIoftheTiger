cdcff64c96d6d61446b59e0fdfd20270e501f0db
JacobRSchwartzCEO <management@schwartztutoring.com>
2020-06-08 15:00:09 -0400

    Fixed Data Alignment Problem
    Boom

--- Video_Score_Tool.py
+++ Video_Score_Tool.py
@@ -8,20 +8,30 @@ import os
 init_time = time.time()
 
 #Live stream of 1s and 0s every
-def record_data(dir,seconds,woods):
-
+def record_data(dir,ideal_seconds,woods):
+    seconds = ideal_seconds - 0.00088
     list = os.listdir(dir)      # dir is your directory path
     total_frames = len(list)    # list of video frames
     data = [[],[]]              # Data structure is a nested list AKA list of lists
     print(woods)                # Prints the initial value of woods, won't change until "S" is pressed
     woods_prev = woods          # Essentially copies woods, will be used to see if a change has occurred.
 
-    for i in range(0, total_frames):
+    error = 0
+    i = 0
+
+
+    while i < total_frames:
+
+
         start_time = time.time()
 
         #appends woods and start_time to  the beginning of the list.
         data[0].append(woods)
         data[1].append(start_time)
+
+        if i > 0:
+            error += (data[1][i] - data[1][i-1] - ideal_seconds)
+
 
         #If a change has occurred print the updated value of woods and reset equality
         if woods_prev != woods:
@@ -37,6 +47,21 @@ def record_data(dir,seconds,woods):
             current_time = time.time()
             if start_time + seconds <= current_time:
                 break
+
+
+        print(error)
+        if error >= ideal_seconds:
+            data[0].append(woods)
+            data[1].append(start_time)
+            i += 1
+            error -= ideal_seconds
+            print("Positive correction error when i=" + str(i))
+
+        elif error <= -1*ideal_seconds:
+            time.sleep(abs(error))
+            print("Negative correction error when i=" + str(i))
+
+        i += 1
     return data
 
 
@@ -58,15 +83,15 @@ print("Go!")
 
 #Inital Variables
 dir = r'C:\Users\manag\PycharmProjects\AIoftheTiger\data_1'
-seconds = 0.0993
+ideal_seconds = 0.1
 woods = 0
 
-tiger_tracker = record_data(dir, seconds, woods)
+tiger_tracker = record_data(dir, ideal_seconds, woods)
 
 print("Done recording data, now writing to CSV")
 
 #Output file
-filepath = "image_score_testycles.csv"
+filepath = "image_score_testing2.csv"
 
 write_to_file(filepath, tiger_tracker)
 
