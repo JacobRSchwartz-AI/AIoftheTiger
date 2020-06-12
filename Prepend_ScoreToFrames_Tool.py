f19b4dac7f28377a0b9b9cb21ea4d7e1b96565fd
JacobRSchwartzCEO <management@schwartztutoring.com>
2020-06-12 15:26:44 -0400

    Updated Python Files

--- Prepend_ScoreToFrames_Tool.py
+++ Prepend_ScoreToFrames_Tool.py
@@ -3,26 +3,45 @@ import os
 import pandas as pd
 import shutil
 
+#Takes a list of lists and writes  it to a csv file.
+def auto_write_to_file(filepath: str, dir):
+    with open(filepath, "w") as file:
+        for x in range(0, len(os.listdir(dir))):
+            file.write("{}\n".format(0))
+
+
+
 #Folder that contains Split out images
-data_file = 'data_1'
+video = '2019 PGA Championship - Final Round'
+
+filepath = video + " Data.csv"
+
+data_file = video + ' Folder'
+
+dir = "C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\" + data_file  # Folder with frames of video we're pulling from
+
+auto_write_to_file(filepath, dir)
 
 #Makes copy of folder
-src = "C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\" +  data_file
-dst = "C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\scored_" + data_file
+src = "C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\" + data_file
+dst = "C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\Scored Data\\scored_" + data_file
+print("Copying Data")
 shutil.copytree(src, dst)
 
 
 #Locates scores for
-loc = ("C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\image_score_" + data_file + ".csv")
+loc = ("C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\" + filepath)
 
 #Reads in the image score file to create a df of the binary digits.
 df = pd.read_csv(loc, header=None, index_col=False)
 
 #For each score, attach it to the end of the name of the frame its associated with.
 for label in range(0, len(df)):
+    if label % round(len(df)/100) == 0:
+        print(str(int(label/round(len(df)/100))) + "% done")
     img_score = df.iloc[label, 0]
-    os.rename(r'C:\Users\manag\PycharmProjects\AIoftheTiger\scored_data_1\frame' + str(label+1) + '.jpg',
-              "C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\scored_data_1\\" + str(label+1) + '_frame_' + str(img_score) + '.jpg')
+    os.rename("C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\Scored Data\\scored_" + data_file + "\\frame" + str(label+1) + '.jpg',
+              "C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\Scored Data\\scored_" + data_file + "\\" + str(label+1) + "_frame_" + str(img_score) + '.jpg')
 
 
 
--- Video_Splitter_Tool.py
+++ Video_Splitter_Tool.py
@@ -2,49 +2,65 @@
 import cv2
 import os
 
-# Read the video from specified path
-cam = cv2.VideoCapture("C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\KetoPizza.MOV")
 
-#folder for output
-folder = 'data_1'
+def video_splitter(cam, folder):
+    try:
 
-try:
+        # creating a folder named folder
+        if not os.path.exists(folder):
+            os.makedirs(folder)
 
-    # creating a folder named folder
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+        # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
 
-    # if not created then raise error
-except OSError:
-    print('Error: Creating directory of data')
+    # frame
+    currentframe = 0
 
-# frame
-currentframe = 0
+    while True:
 
-while True:
+        # reading from frame
+        ret, frame = cam.read()
 
-    # reading from frame
-    ret, frame = cam.read()
+        if ret:
+            # if video is still left continue creating images
+            name = './' + folder + '/frame' + str(int(1+currentframe/3)) + '.jpg'
 
-    if ret:
-        # if video is still left continue creating images
-        name = './' + folder + '/frame' + str(int(1+currentframe/3)) + '.jpg'
+            if currentframe % 3 == 0:
+                if currentframe % 300 == 0:
+                    os.system('cls')
+                    print('Creating...' + name)
+                # writing the extracted images
+                cv2.imwrite(name, frame)
 
-        #Record number of  frames per second
-        fps = 30
-        division_factor = fps / 10
-        if currentframe % division_factor == 0:
-            print('Creating...' + name)
-            # writing the extracted images
-            cv2.imwrite(name, frame)
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
 
-        # increasing counter so that it will
-        # show how many frames are created
-        currentframe += 1
+        else:
+            break
 
-    else:
-        break
+    # Release all space and windows once done
+    cam.release()
+    cv2.destroyAllWindows()
 
-# Release all space and windows once done
-cam.release()
-cv2.destroyAllWindows()
+
+unsplit_video_files = [
+
+                        "Tony Finau Highlights _ Round 2 _ 2018 WGC",
+                        "Tony Finau shoots 9-under 62 _ Round 3 _ Waste Management 2020",
+                        "Tony Finau shoots 10-under 62 _ Round 2 _ The American Express 2020",
+                        "Tony Finau's Highlights _ Round 3 _ Hero World Challenge 2018",
+                        "Tony Finau's Third Round in Three Minutes"
+
+                      ]
+
+
+for video in range(0,len(unsplit_video_files)):
+    # Read the video from specified path
+    cam = cv2.VideoCapture("C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\" + unsplit_video_files[video] + ".mp4")
+
+    #folder for output
+    folder = unsplit_video_files[video] + ' Folder'
+
+    video_splitter(cam, folder)
