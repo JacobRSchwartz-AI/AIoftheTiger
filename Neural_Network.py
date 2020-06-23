30ca22c6d487ad1f43cd7469f41632d1a2624596
JacobRSchwartzCEO <66438270+JacobRSchwartzCEO@users.noreply.github.com>
2020-06-23 14:04:00 -0400

    Neural Network Updated

--- Neural_Network.py
+++ Neural_Network.py
@@ -13,44 +13,46 @@ import numpy as np
 f = open("directory.txt", "r")
 path = f.read()
 
-folder_name = path + "\\Scored Data\\scored_TW Memorial Round 3 2018 Folder"
-print(folder_name)
-folder = os.listdir(folder_name)
+train_folder_name = path + "\\Scored Data\\scored_TW Memorial Round 3 2018 Folder"
+print(train_folder_name)
+train_folder = os.listdir(train_folder_name)
 
-print(len(folder))
+test_folder_name = path + "\\Scored Data\\Quick Test"
+print(test_folder_name)
+test_folder = os.listdir(test_folder_name)
 
-total_images = len(folder)
-num_train_images = round(0.8*total_images)
-num_test_images = total_images - num_train_images
 
-train_images = np.zeros((num_train_images,144,256,3))
-train_labels = np.zeros(num_train_images)
-test_images = np.zeros((num_test_images,144,256,3))
-test_labels = np.zeros((num_test_images))
+num_train_images = len(train_folder)
+num_test_images = len(test_folder)
 
-training_proportion = 0.8
+train_images = np.zeros((num_train_images,144,256,3), dtype='float16')
+train_labels = np.zeros((num_train_images, 1), dtype='i4')
+test_images = np.zeros((num_test_images,144,256,3), dtype='float16')
+test_labels = np.zeros((num_test_images, 1), dtype='i4')
+
+print(test_labels)
+
+# training_proportion = 0.8
 test_frame = 0
 train_frame = 0
 
-for frame in range(0, len(folder)):
-    frame_path = folder_name + "\\" + folder[frame]
+for frame in range(0, len(train_folder)):
+    frame_path = train_folder_name + "\\" + train_folder[frame]
     ResizeImages.resizeImage(256, 144, frame_path)
     image = cv2.imread(frame_path)
-    if frame%5 != 4:
-        if frame == len(folder)-1 and (len(folder)%5 == 3 or len(folder)%5 == 4):
-            test_images[num_test_images-1]= image
-            test_labels[num_test_images-1] = (int(folder[frame][-5]))
-        else:
-            train_images[train_frame] = image
-            train_labels[train_frame] = (int(folder[frame][-5]))
-            train_frame += 1
-    else:
-        test_images[test_frame] = image
-        test_labels[test_frame] = (int(folder[frame][-5]))
-        test_frame += 1
+    train_images[frame] = image
+    train_labels[frame][0] = (int(train_folder[frame][-5]))
+    if frame % round(len(train_folder) / 100) == 0:
+        print(str(int(frame / round(len(train_folder) / 100))) + "% done adding to training array")
 
-    if frame % round(len(folder) / 100) == 0:
-        print(str(int(frame / round(len(folder) / 100))) + "% done adding to lists")
+for frame in range(0, len(test_folder)):
+    frame_path = test_folder_name + "\\" + test_folder[frame]
+    ResizeImages.resizeImage(256, 144, frame_path)
+    image = cv2.imread(frame_path)
+    test_images[frame] = image
+    test_labels[frame][0] = (int(test_folder[frame][-5]))
+    if frame % round(len(test_folder) / 100) == 0:
+        print(str(int(frame / round(len(test_folder) / 100))) + "% done adding to testing array")
 
 
 
@@ -76,18 +78,18 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 
 class_names = ['Not Tiger', 'Tiger']
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    # The CIFAR labels happen to be arrays, 
-    # which is why you need the extra index
-    # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    plt.xlabel(class_names[train_labels[i][0]])
-plt.show()
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     # The CIFAR labels happen to be arrays, 
+#     # which is why you need the extra index
+#     # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#     plt.xlabel(class_names[train_labels[i][0]])
+# plt.show()
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(144, 256, 3)))
