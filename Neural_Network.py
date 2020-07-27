32f4f2503ca8dbde45a1266f41e26d20a582c897
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-07-27 11:49:33 -0700

    Clean up AI Tiger

---
+++ Neural_Network.py
@@ -0,0 +1,178 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models, optimizers
+import matplotlib.pyplot as plt
+import cv2
+import os, os.path
+import random
+import numpy as np
+import keras
+import Functions
+
+
+# ResizeImages.resizeImage(256, 144, "C:\\Users\\manag\\PycharmProjects\\AIoftheTiger\\Scored Data\\scored_TW Memorial Round 3 2018 Folder\\1_frame_1.jpg")
+
+f = open("directory.txt", "r")
+path = f.read()
+
+
+scored_data_name = path + "\\Scored Data\\"
+scored_data_folder = os.listdir(scored_data_name)
+
+train_folders_list = []
+test_folders_list = []
+
+for folder in range(0,len(scored_data_folder)):
+    if folder % 5 != 3:
+        train_folders_list.append(path + "\\Scored Data\\" + scored_data_folder[folder])
+    else:
+        test_folders_list.append(path + "\\Scored Data\\" + scored_data_folder[folder])
+
+num_train_images = 0
+num_test_images = 0
+
+for folder in range(0,len(train_folders_list)):
+    if len(os.listdir(train_folders_list[folder])) >= 40000:
+        num_train_images += 40000
+    else:
+        num_train_images += len(os.listdir(train_folders_list[folder]))
+
+for folder in range(0,len(test_folders_list)):
+    num_test_images += len(os.listdir(test_folders_list[folder]))
+
+height_px = 144
+width_px = 256
+
+train_images = np.zeros((num_train_images,height_px,width_px,3), dtype='float64')
+train_labels = np.zeros((num_train_images, 1), dtype='i4')
+test_images = np.zeros((num_test_images,height_px,width_px,3), dtype='float64')
+test_labels = np.zeros((num_test_images, 1), dtype='i4')
+
+# training_proportion = 0.8
+test_frame = 0
+train_frame = 0
+train_index = 0
+test_index = 0
+
+for folder in range(0,len(train_folders_list)):
+    train_folder = os.listdir(train_folders_list[folder])
+    print("Pulling images from: " + train_folders_list[folder] + " to add to training data")
+    for frame in range(0, len(train_folder)):
+        frame_path = train_folders_list[folder] + "\\" + train_folder[frame]
+        ResizeImages.resizeImage(width_px, height_px, frame_path)
+        image = cv2.imread(frame_path)
+        train_images[train_index] = image
+        train_labels[train_index][0] = int(train_folder[frame][-5]) - 1
+        train_index += 1
+        if frame % round(len(train_folder) / 100) == 0:
+            print(str(int(frame / round(len(train_folder) / 100))) + "% done adding to training array")
+        if frame == 39999:
+            break
+
+for folder in range(0,len(test_folders_list)):
+    test_folder = os.listdir(test_folders_list[folder])
+    print("Pulling images from: " + test_folders_list[folder] + " to add to testing data")
+    for frame in range(0, len(test_folder)):
+        frame_path = test_folders_list[folder] + "\\" + test_folder[frame]
+        ResizeImages.resizeImage(width_px, height_px, frame_path)
+        image = cv2.imread(frame_path)
+        test_images[test_index] = image
+        test_labels[test_index][0] = int(test_folder[frame][-5]) - 1
+        test_index += 1
+        if frame % round(len(test_folder) / 100) == 0:
+            print(str(int(frame / round(len(test_folder) / 100))) + "% done adding to testing array")
+
+
+
+# (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# print(len(train_images))
+# print(len(train_images[0]))
+# print(len(train_images[0][0]))
+# print(len(train_images[0][0][0]))
+# print(len(train_images[0][0][0][0]))
+
+
+#print(len(train_images[0][0]))
+
+# train_images = np.array(train_images)
+# train_labels = np.array(train_labels)
+# test_images =  np.array(test_images)
+# test_labels = np.array(test_labels)
+
+
+# Normalize pixel values to be between 0 and 1
+
+train_images, test_images = train_images / 255.0, test_images / 255.0
+class_names = ['Tiger-Half', 'Tiger-Full', 'Blue/Grey Background', 'Green/Yellow Background', 'Other golfer-half', 'Other golfer-full']
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    # plt.imshow(cv2.cvtColor(train_images[i], cv2.COLOR_BGR2RGB))
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    # The CIFAR labels happen to be arrays, 
+    # which is why you need the extra index
+    # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (9, 9), activation='relu', input_shape=(height_px, width_px, 3), padding='same'))
+# model.add(layers.Conv2D(32, (9, 9), activation='relu', padding='same'))
+# model.add(layers.Conv2D(32, (9, 9), activation='relu', padding='same'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Dropout(0.5, noise_shape=None, seed=None))
+# model.add(layers.Conv2D(32, (7, 7), activation='relu', padding='same'))
+# model.add(layers.Conv2D(32, (7, 7), activation='relu', padding='same'))
+# model.add(layers.Conv2D(32, (5, 5), activation='relu', padding='same'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Dropout(0.5, noise_shape=None, seed=None))
+# model.add(layers.Conv2D(32, (5, 5), activation='relu', padding='same'))
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+# model.add(layers.Conv2D(32, (2, 2), activation='relu', padding='same'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(32, activation='relu'))
+# model.add(layers.Dense(8))
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(height_px, width_px, 3), padding='same'))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(layers.Dropout(0.4, noise_shape=None, seed=None))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(layers.Dropout(0.4, noise_shape=None, seed=None))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(layers.Dropout(0.4, noise_shape=None, seed=None))
+model.add(layers.Flatten())
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(6))
+model.summary()
+
+# opt = tf.keras.optimizers.SGD(learning_rate=0.05, momentum=0.9)
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# my_callbacks = [
+#     tf.keras.callbacks.EarlyStopping(patience=2),
+#     tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5'),
+#     tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+# ]
+
+model.fit(train_images, train_labels, batch_size=32, epochs=10, validation_data=(test_images, test_labels))
+
+# plt.plot(history.history['accuracy'], label='accuracy')
+# plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.ylim([0.5, 1])
+# plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
---
+++ Unused Python/Video_Score_Tool.py
@@ -0,0 +1,183 @@
+import time
+import keyboard
+import os
+import cv2
+import pandas as pd
+import shutil
+import Functions
+
+
+#When Program Starts
+init_time = time.time()
+
+f = open("directory.txt", "r")
+path = f.read()
+
+#Live stream of 1s and 0s every
+def record_data(dir,video,woods):
+    fps = video.get(cv2.CAP_PROP_FPS)
+    print(fps)
+    ideal_seconds = 1/(fps/3)
+    seconds = ideal_seconds
+    list = os.listdir(dir)      # dir is your directory path
+    total_frames = len(list)    # list of video frames
+    data = [[],[]]              # Data structure is a nested list AKA list of lists
+    print(woods)                # Prints the initial value of woods, won't change until "S" is pressed
+    woods_prev = woods          # Essentially copies woods, will be used to see if a change has occurred.
+
+    error = 0
+    i = 0
+
+    while i < total_frames:
+        start_time = time.time()
+
+        #appends woods and start_time to  the beginning of the list.
+        data[0].append(woods)
+        data[1].append(start_time)
+
+        if i > 0:
+            error += (data[1][i] - data[1][i-1] - ideal_seconds)
+
+        # If a change has occurred print the updated value of woods and reset equality
+        if woods_prev != woods:
+            print(woods)
+            woods_prev = woods
+
+        #On button  press change the stream of outgoing 0s to 1s and change 1s to 0s.
+        elif keyboard.is_pressed("S"):
+            woods = (woods+1) % 2
+
+        #Loop designed to slow the program down to 10 iterations per second.
+        while True:
+            current_time = time.time()
+            if start_time + seconds <= current_time:
+                break
+
+        #print(error)
+        if error >= ideal_seconds:
+            data[0].append(woods)
+            data[1].append(start_time)
+            i += 1
+            error -= ideal_seconds
+            seconds = seconds*0.998
+            #print("Positive correction error when i=" + str(i))
+
+        elif error <= -1*ideal_seconds:
+            time.sleep(abs(error))
+            #print("Negative correction error when i=" + str(i))
+            seconds = seconds*1.002
+
+        i += 1
+    return data
+
+
+#Takes a list of lists and writes  it to a csv file.
+def write_to_file(filepath: str, data):
+    with open(filepath, "w") as file:
+        for x in range(0,len(data[0])):
+            file.write("{},{}\n".format(data[0][x],data[1][x]))
+
+
+def auto_write_to_file(filepath: str, dir):
+    with open(filepath, "w") as file:
+        for x in range(0, len(os.listdir(dir))):
+            if x % round(len(os.listdir(dir)) / 100) == 0:
+                print(str(int(x / round(len(os.listdir(dir)) / 100))) + "% done with writing to CSV file")
+            file.write("{}\n".format(0))
+
+
+video_files = [
+
+                    "Brooks Koepkas highlights Round 1 3M",
+                    "Brooks Koepka's highlights _ Round 2 _ TOUR Championship 2019-FwG4GzJjC8s",
+                    "Bryson DeChambeau Rocket Mortgage Classic",
+                    "Dustin Johnson WGCMexico",
+                    "Jon Rahm shoots 7-under 65 _ Round 3 _ Farmers 2020-ASvCXGI4ark",
+                    "Jordan Spieth's winning highlights from 2017 AT&T Pebble Beach Pro-Am-PTXpmjlI3yg",
+                    "Justin Thomas' Winning Highlights From The 2020 Sentry Tournament of Champions-z_lzGbUKhmY",
+                    "Patrick Reed wins the 2020 WGC-Mexico Championship _ Extended Highlights-vQwSVE7gjBM",
+                    "Phil Mickelson shoots 5-under 67 _ Round 3 _ AT&T Pebble Beach 2020-rDk2vx45_CQ",
+                    "Phil Mickelson shoots 7-under 63 _ Round 2 _ Travelers Championship 2020-KW4xetuxjqY",
+                    "Rickie Fowlers BMW Championship",
+                    "Rory McIlroy shoots 7-under 63 _ Round 2 _ Charles Schwab Challenge-MIe8lPGqxIU",
+                    "Rory McIlroy's winning highlights from TOUR Championship 2019-jSS-_Iu0Qkw",
+
+                      ]
+for video in range(0,len(video_files)):
+    woods = 1   # Initial value of first frame
+    video_file = video_files[video] + ".mp4"
+
+    #Inital Variables
+    folder = video_file[:-4] + ' Folder'
+    dir = path + folder  # Folder with frames of video we're pulling from
+    video = cv2.VideoCapture(path + video_file) # From original video find fps
+
+    automatic_manual = int(input("Enter 0 to automatically score with all 0s and 1 to manually score: "))
+    #automatic_manual = 0
+
+    #Output file of scores
+    data_scores = video_file[:-4] + " Data.csv"
+
+    if automatic_manual == 1:
+        # Start of execution, allows for 3 seconds from starting this program to starting a video to score.
+        print("On your marks")
+        time.sleep(1)
+        print("Get Ready")
+        time.sleep(1)
+        print("Get Set")
+        time.sleep(1)
+        print("Go!")
+        tiger_tracker = record_data(dir, video, woods)
+
+        print("Done recording data, now writing to CSV")
+        write_to_file(data_scores, tiger_tracker)
+    else:
+        print("Now auto-scoring")
+        auto_write_to_file(data_scores, dir)
+
+
+
+    #Folder that contains Split out images
+    img_folder = video_file[:-4] + ' Folder'
+
+    #Makes copy of folder
+    src = path +  img_folder
+    dst = path + "Scored Data\\scored_" + img_folder
+    print("Copying Data")
+
+    try:
+        shutil.copytree(src, dst)
+    except FileExistsError:
+        print("Folder " + dst + " already exists")
+        decision = int(input("Enter 0 to overwrite and 1 to keep additional scored copies: "))
+        if decision == 0:
+            shutil.rmtree(dst)
+            shutil.copytree(src, dst)
+            print("Overwriting Data")
+        else:
+            dst += str(2)
+            shutil.copytree(src, dst)
+            print("Copying Data")
+
+
+
+    #Locates scores for
+    loc = (path + data_scores)
+
+    #Reads in the image score file to create a df of the binary digits.
+    df = pd.read_csv(loc, header=None, index_col=False)
+
+    #For each score, attach it to the end of the name of the frame its associated with.
+    for label in range(0, len(df)):
+        if label % round(len(df)/100) == 0:
+            print(str(int(label/round(len(df)/100))) + "% done")
+        img_score = df.iloc[label, 0]
+        os.rename(dst + "\\frame" + str(label+1) + '.jpg',
+                dst + "\\" + str(label+1) + "_frame_" + str(img_score) + '.jpg')
+
+
+    reaction_time_fixer(dst, 256, 144)
+
+end_time = time.time()
+
+print(end_time - init_time)
